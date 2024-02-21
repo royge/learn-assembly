b8021e8654d46ffa0a2f07e71cf8057c96b67517
Roy Evangelista <royevangelista@gmail.com>
2024-02-21 16:04:20 +0800

    Add comments

--- lower.s
+++ lower.s
@@ -1,28 +1,28 @@
 .global _start
 
 _start:
-	LDR X2, =input
-	LDR X3, =result
+	LDR X2, =input // Load the address of the input string.
+	LDR X3, =result // Load the address of the result string.
 loop:
-	LDRB W4, [X2], #1
-	CMP W4, #'Z'
-	B.GT cont
+	LDRB W4, [X2], #1 // Load a byte from the input string.
+	CMP W4, #'Z' // Check if the byte is a capital letter.
+	B.GT cont // If not, continue to the next byte.
 
-	CMP W4, #'A'
-	B.LT cont
+	CMP W4, #'A' // Check if the byte is a capital letter.
+	B.LT cont // If not, continue to the next byte.
 
-	ADD W4, W4, #('a'-'A')
+	ADD W4, W4, #('a'-'A') // Convert the capital letter to a small letter.
 cont:
-	STRB W4, [X3], #1
+	STRB W4, [X3], #1 // Store the byte in the result string.
 
-	CMP W4, #0
-	B.NE loop
+	CMP W4, #0 // Check if the byte is the null terminator.
+	B.NE loop // If not, continue to the next byte.
 
-	MOV X0, #1
-	LDR X1, =result
-	SUB X2, X3, X1
-	MOV X8, #64
-	SVC #0
+	MOV X0, #1 // Write.
+	LDR X1, =result // Load the address of the result string.
+	SUB X2, X3, X1 // Calculate the length of the result string.
+	MOV X8, #64 // Linux write function.
+	SVC #0 // Make linux system call.
 
 	MOV X0, X5 // Exit.
 	MOV X8, #93 // Linux exit function.
